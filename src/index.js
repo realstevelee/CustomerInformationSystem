57885e3ec54cfcf916651d9f4618aefa5fdfeefb
realstevelee <realstevelee@gmail.com>
2020-07-03 01:39:58 -0400

    1st commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
+/*********************************************************************************
+* WEB422 – Assignment 3
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+*
+* Name: Sooyong Lee Student ID: 146074174 Date: June 25, 2020
+* 
+*
+********************************************************************************/
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
